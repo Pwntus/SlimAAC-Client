55816ff38cda222af7dc922a2eb70d43ba6641f2
Pwntus <pontus.edvard@gmail.com>
2016-12-30 22:52:31 +0100

    Component import refactor

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import News from './pages/News'
+import Downloads from './pages/Downloads'
+import Register from './pages/account/Register'
+import Login from './pages/account/Login'
+import Logout from './pages/account/Logout'
+import NotFound from './pages/NotFound'
+
+export default {
+	News,
+	Downloads,
+	Register,
+	Login,
+	Logout,
+	NotFound	
+}
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,11 @@
-import News from '../components/pages/News'
-import Downloads from '../components/pages/Downloads'
-import Register from '../components/pages/account/Register'
-import Login from '../components/pages/account/Login'
-import Logout from '../components/pages/account/Logout'
-import NotFound from '../components/pages/NotFound'
+import C from '../components'
 
 export default [
-	{ path: '/', 					component: News, 		meta: {authLevel: 0} },
-	{ path: '/news', 				component: News, 		meta: {authLevel: 0} },
-	{ path: '/downloads', 			component: Downloads, 	meta: {authLevel: 0} },
-	{ path: '/account/register', 	component: Register, 	meta: {authLevel: -1} },
-	{ path: '/account/login', 		component: Login, 		meta: {authLevel: -1} },
-	{ path: '/account/logout', 		component: Logout, 		meta: {authLevel: 0} },
-	{ path: '*', 					component: NotFound, 	meta: {authLevel: 0} }
+	{ path: '/', 					component: C.News, 		meta: {authLevel: 0} },
+	{ path: '/news', 				component: C.News, 		meta: {authLevel: 0} },
+	{ path: '/downloads', 			component: C.Downloads, meta: {authLevel: 0} },
+	{ path: '/account/register', 	component: C.Register, 	meta: {authLevel: -1} },
+	{ path: '/account/login', 		component: C.Login, 	meta: {authLevel: -1} },
+	{ path: '/account/logout', 		component: C.Logout, 	meta: {authLevel: 0} },
+	{ path: '*', 					component: C.NotFound, 	meta: {authLevel: 0} }
 ]
